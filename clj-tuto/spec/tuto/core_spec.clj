aa80defc47e37b4a9a47e46a98b5657800b44cfd
bouhaniHamdi <bouhanihamdi@gmail.com>
2023-03-14 15:29:54 +0100

    .

--- clj-tuto/spec/tuto/core_spec.clj
+++ clj-tuto/spec/tuto/core_spec.clj
@@ -10,10 +10,8 @@
               (should= 1 (my-reduce + 1 '()))))
 
 (describe "Truth"
-
           (it "is true"
               (should true))
-
           (it "is not false"
               (should-not false)))
 
--- clj-tuto/test/tuto/test.clj
+++ clj-tuto/test/tuto/test.clj
@@ -1,1 +1,15 @@
-(ns tuto.test)
+(ns tuto.test
+  (:require [clojure.test :as t :refer [deftest is testing]]))
+
+(deftest math-works
+  (testing "basic addition and subtraction"
+    (is (= 42 (+ 40 2)))
+    (is (= 42 (- 44 2)))))
+
+(deftest ^:integration test-i
+  (is (= 1 1)))
+
+;; (deftest ^:integration test-live-system
+;;   (is (= 200 (:status (http/get "http://example.com")))))
+
+(math-works)
